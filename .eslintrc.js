5d46999faf56ee163d422f175c923d20f687c1ae
Eanna Glavin <eannagglavin@gmail.com>
2022-11-06 00:01:16 +0000

    chore: add eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,85 @@
+/**
+ * {@link https://eslint.org/docs/user-guide Eslint User Guide}
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      pragma: "React",
+      version: "detect",
+    },
+  },
+
+  ignorePatterns: ["/dist", "/node_modules", "*.test.*.snap"],
+
+  plugins: ["react", "jsx-a11y", "prettier", "@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+  ],
+
+  /**
+   * https://eslint.org/docs/user-guide/configuring/rules#configuring-rules
+   *
+   * "off" or 0 - turn the rule off
+   * "warn" or 1 - turn the rule on as a warning (doesn't affect exit code)
+   * "error" or 2 - turn the rule on as an error (exit code is 1 when triggered)
+   */
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "@typescript-eslint/no-empty-function": 1,
+    "@typescript-eslint/no-unused-vars": 0,
+    "no-console": 0,
+    "no-constant-condition": 1,
+    "no-empty-function": 1,
+    "no-unused-vars": 0,
+    "prettier/prettier": [1, { endOfLine: "auto" }],
+    "react/display-name": 0,
+    "react/jsx-uses-react": 0,
+    "react/react-in-jsx-scope": 0,
+  },
+
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      rules: {
+        "@typescript-eslint/explicit-module-boundary-types": 1,
+        "@typescript-eslint/no-empty-interface": 0,
+        "@typescript-eslint/no-explicit-any": 1,
+        "@typescript-eslint/no-non-null-assertion": 0,
+        "@typescript-eslint/no-unused-vars": [
+          1,
+          { ignoreRestSiblings: true, argsIgnorePattern: "^_" },
+        ],
+      },
+    },
+    {
+      files: ["**/*.js", "**/*.jsx"],
+      rules: {
+        "no-unused-vars": [1, { ignoreRestSiblings: true, argsIgnorePattern: "^_" }],
+      },
+    },
+  ],
+};
--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
-const packageJson = require("./package.json");
+import packageJson from "./package.json";
 
 export default [
   {
@@ -20,11 +20,7 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-    ],
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
   },
   {
     input: "dist/esm/types/index.d.ts",
